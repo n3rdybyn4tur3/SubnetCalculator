f6ea9e5c8ab49a1cc50f674f820ba3b9b28eed84
n3rdybyn4tur3 <j.lexau@stud.bhh.de>
2023-10-06 11:31:35 +0200

    FEAT: resolveIPv4 & IPv4Address

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,6 +1,36 @@
-fun resolveIPv4 (address: String): Array<Int> {
+
+class Octet (var octet: Int)
+class IPv4Address (var address: MutableList<Octet>, var netmask: Int){
+    fun printInfo () {
+        print("netmask: ")
+        println(netmask)
+        println("address (octets): ")
+        print("     ")
+        var counter = 0
+        for (i in address) {
+            print(i.octet)
+            if (counter < 3) {
+                print(".")
+            }
+            counter += 1
+        }
+        println()
+    }
+}
+
+fun resolveIPv4 (address: String): IPv4Address {
     println(address)
-    return arrayOf(123, 45, 67, 89)
+    val splits: List<String> = address.split("/")
+    val ipv4: String = splits.first()
+    val strNetmask: String = splits.last()
+    val netmask = strNetmask.toInt()
+    val strOctets: List<String> = ipv4.split(".")
+    var octets: MutableList<Octet> = mutableListOf()
+    for (octet in strOctets) {
+        octets.add(element = Octet(octet.toInt()))
+    }
+
+    return IPv4Address(address = octets, netmask= netmask)
 }
 
 
@@ -14,9 +44,9 @@ fun resolveIPv4 (address: String): Array<Int> {
 
 fun main() {
     println("Let's create a subnet calculator")
-    for (octet in resolveIPv4("test")) {
-        println(octet)
-    }
+    var myAddress = resolveIPv4("192.168.0.1/24")
+    println("Return")
+    myAddress.printInfo()
 }
 
 
