15d9b1b161c1609ba40f2e7b5cb8d92f1347da78
n3rdybyn4tur3 <j.lexau@stud.bhh.de>
2023-10-08 20:18:48 +0200

    FEAT: CIDR to Subnet mask

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,19 +1,36 @@
 
 class Octet (var octet: Int)
-class IPv4Address (var address: MutableList<Octet>, var netmask: Int){
-    fun printInfo () {
-        print("netmask: ")
-        println(netmask)
-        println("address (octets): ")
-        print("     ")
-        var counter = 0
-        for (i in address) {
-            print(i.octet)
-            if (counter < 3) {
-                print(".")
-            }
-            counter += 1
+class IPv4Address (var address: List<Octet>, var CIDR: Int){
+
+    val subentmask = cidrToSubnetmask(CIDR)
+
+
+    fun cidrToSubnetmask(CIDR: Int): Array<Int> {
+        var mask: Array<Int> = arrayOf(0,0,0,0)
+        for (i in 0..<CIDR) {
+            mask[i/8] = mask[i/8] + (1 shl (7-i%8))
         }
+        return mask
+    }
+    fun printInfo () {
+        var strAddress: String= ""
+        for (i in address) {
+            strAddress += i.octet
+            strAddress += "."
+        }
+        strAddress.drop(1).dropLast(1)
+        println("address: $strAddress")
+
+        var strSubnetmask: String =""
+        for (i in subentmask) {
+            strSubnetmask += i
+            strSubnetmask += "."
+        }
+        strSubnetmask.drop(1).dropLast(1)
+        println("subnet mask: $strSubnetmask")
+
+        println("CIDR: /$CIDR")
+
         println()
     }
 }
@@ -23,14 +40,14 @@ fun resolveIPv4 (address: String): IPv4Address {
     val splits: List<String> = address.split("/")
     val ipv4: String = splits.first()
     val strNetmask: String = splits.last()
-    val netmask = strNetmask.toInt()
+    val CIDR = strNetmask.toInt()
     val strOctets: List<String> = ipv4.split(".")
-    var octets: MutableList<Octet> = mutableListOf()
+    val octets: MutableList<Octet> = mutableListOf()
     for (octet in strOctets) {
         octets.add(element = Octet(octet.toInt()))
     }
 
-    return IPv4Address(address = octets, netmask= netmask)
+    return IPv4Address(address = octets, CIDR= CIDR)
 }
 
 
@@ -44,9 +61,18 @@ fun resolveIPv4 (address: String): IPv4Address {
 
 fun main() {
     println("Let's create a subnet calculator")
-    var myAddress = resolveIPv4("192.168.0.1/24")
+    val myAddress = resolveIPv4("192.168.0.1/24")
     println("Return")
     myAddress.printInfo()
+    //println()
+    //println()
+    /*
+    for (i: Int in 1..32) {
+        print("Mask /$i: ")
+        cidrToSubnetmask(i)
+        println()
+    }
+    */
 }
 
 
