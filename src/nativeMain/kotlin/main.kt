80987e0eb978b7d10a3bd9839b031ab91bc45a50
n3rdybyn4tur3 <j.lexau@stud.bhh.de>
2023-10-09 06:51:43 +0200

    FEAT: restructure IPv4 class, initialized methods, finished print-Info

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,53 +1,111 @@
 
 class Octet (var octet: Int)
-class IPv4Address (var address: List<Octet>, var CIDR: Int){
+class IPv4Address (private var address: List<Octet>, private var cidr: Int){
 
-    val subentmask = cidrToSubnetmask(CIDR)
+    private val subnetMask = cidrToSubnetMask()
+    private val network = getNetwork()
+    private val wildcardMask = getWildcard()
+    private val broadcast = getBroadcast()
+    private val usableRange = getRange()
+    private val totalHosts = getTotalHosts()
+    private val usableHosts = getUsableHosts()
 
 
-    fun cidrToSubnetmask(CIDR: Int): Array<Int> {
-        var mask: Array<Int> = arrayOf(0,0,0,0)
-        for (i in 0..<CIDR) {
+
+    private fun cidrToSubnetMask(): Array<Int> {
+        val mask: Array<Int> = arrayOf(0,0,0,0)
+        for (i in 0..<cidr) {
             mask[i/8] = mask[i/8] + (1 shl (7-i%8))
         }
         return mask
     }
+
+
+    private fun getNetwork(): Array<Int> {
+        return arrayOf(0,0,0,0)
+    }
+
+
+    private fun getWildcard(): Array<Int> {
+        return arrayOf(0,0,0,0)
+    }
+
+    private fun getBroadcast(): Array<Int> {
+        return arrayOf(0,0,0,0)
+    }
+
+
+    private fun getRange(): String {
+        return "0.0.0.0 - 255.255.255.255"
+    }
+
+
+    private fun getTotalHosts(): Int {
+        return 0
+    }
+
+
+    private fun getUsableHosts(): Int {
+        return 0
+    }
+
+
+
     fun printInfo () {
-        var strAddress: String= ""
+        var strAddress= ""
         for (i in address) {
             strAddress += i.octet
             strAddress += "."
         }
-        strAddress.drop(1).dropLast(1)
+        strAddress = strAddress.dropLast(1)
         println("address: $strAddress")
 
-        var strSubnetmask: String =""
-        for (i in subentmask) {
-            strSubnetmask += i
-            strSubnetmask += "."
-        }
-        strSubnetmask.drop(1).dropLast(1)
-        println("subnet mask: $strSubnetmask")
+        val strNetwork = printOctets(network)
+        println("network address: $strNetwork")
 
-        println("CIDR: /$CIDR")
+        println("CIDR: /$cidr")
+
+        val strSubnetMask = printOctets(subnetMask)
+        println("subnet mask: $strSubnetMask")
+
+        val strWildcardMask = printOctets(wildcardMask)
+        println("wildcard mask: $strWildcardMask")
+
+        val strBroadcast = printOctets(broadcast)
+        println("broadcast address: $strBroadcast")
+
+        println("Usable Host Range: $usableRange")
+        println("Total Hosts: $totalHosts")
+        println("Usable Hosts: $usableHosts")
 
         println()
     }
 }
+
+fun printOctets(octets: Array<Int>): String {
+    var result = ""
+    for (i in octets) {
+        result += i
+        result += "."
+    }
+
+    return result.dropLast(1)
+}
+
 
 fun resolveIPv4 (address: String): IPv4Address {
     println(address)
     val splits: List<String> = address.split("/")
     val ipv4: String = splits.first()
     val strNetmask: String = splits.last()
-    val CIDR = strNetmask.toInt()
+    val cidr = strNetmask.toInt()
     val strOctets: List<String> = ipv4.split(".")
     val octets: MutableList<Octet> = mutableListOf()
     for (octet in strOctets) {
         octets.add(element = Octet(octet.toInt()))
     }
 
-    return IPv4Address(address = octets, CIDR= CIDR)
+    return IPv4Address(address = octets, cidr = cidr)
 }
 
 
@@ -62,17 +120,8 @@ fun resolveIPv4 (address: String): IPv4Address {
 fun main() {
     println("Let's create a subnet calculator")
     val myAddress = resolveIPv4("192.168.0.1/24")
-    println("Return")
+
     myAddress.printInfo()
-    //println()
-    //println()
-    /*
-    for (i: Int in 1..32) {
-        print("Mask /$i: ")
-        cidrToSubnetmask(i)
-        println()
-    }
-    */
 }
 
 
